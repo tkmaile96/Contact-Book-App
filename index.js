9ccf351f228fc8f72eefe542299607a47c2393ae
Teekay <tkmaile96@gmail.com>
2025-06-16 17:25:32 +0200

    add contact button updated

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
 
+// refresh button
+const refreshButton = document.getElementById('refresh').addEventListener("click", fetchContacts);
+
+// Add contacts button
+const addContacts = document.getElementById('addContacts').addEventListener("click", addContacts);
+
+ //Add contacts function
+function addContacts() {
+    // create a new conatct
+const newContact = {
+    picture: {
+             large:"https://picsum.photos/200/300",
+               },
+    name: { first: "New" , last: "User"},
+    gender: "male",
+    email: "newuser@example.com",
+    phone: "123-456-7890",
+}
+
+};
+
+contacts.push(newContact); // add the new contact to the contacts array
+displayOutput(contacts); // display the updated contacts list
+
 function fetchContacts() {
-    fetch("https://randomuser.me/api/?results=20")
+    fetch("https://randomuser.me/api/?results=2")
     .then(function (response) {
         return response.json();
     })
+
     .then(function(data) {
         console.log(data.data); // this is the array of users
         displayOutput(data.results);
