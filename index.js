88c4ac669e600bf5a47ba6b7406a02566eb41af7
Teekay <tkmaile96@gmail.com>
2025-06-16 17:46:13 +0200

    added and updated the edit button functionality

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 
+let contacts = []; // array to store contact objects
+
 // refresh button
 const refreshButton = document.getElementById('refresh').addEventListener("click", fetchContacts);
 
@@ -16,12 +18,14 @@ const newContact = {
     gender: "male",
     email: "newuser@example.com",
     phone: "123-456-7890",
-}
-
 };
 
 contacts.push(newContact); // add the new contact to the contacts array
 displayOutput(contacts); // display the updated contacts list
+
+};
+
+
 
 function fetchContacts() {
     fetch("https://randomuser.me/api/?results=2")
@@ -38,6 +42,7 @@ function fetchContacts() {
     });
 }
 
+// The Display conatcts function
 function displayOutput(contacts) {
     let table = document.getElementById("table");
     table.innerHTML = "";
@@ -50,8 +55,27 @@ function displayOutput(contacts) {
             <p>Gender: ${contact.gender}</p>
             <p>Email: ${contact.email}</p>
             <p>Phone: ${contact.phone}</p>
+            <button onclick="editContact(${index})">Edit</button>
+            <button onclick="deleteContact(${index})">Delete</button>
             <hr>
         `;
         table.appendChild(contactDiv);
     });
+};
+
+// The Edit Contact function
+function editContact(index) {
+    let newImage = document.createElement("img");
+    let newName = prompt("Enter new first name:", contacts[index].name.first);
+    let newGender = prompt ("Enter new gender:", contacts[index].gender);
+    let newEmail = prompt ("Enter new email:", contacts[index].email);
+    let newPhone = prompt ("Enter new phone:", contacts[index].phone);
+
+    if (newImage) contacts[index].img = newImage;
+    if (newName) contacts[index].name.first = newName;
+    if (newGender) contacts[index].gender = newGender;
+    if (newEmail) contacts[index].email = newEmail;
+    if (newPhone) contacts[index].phone = newPhone;
+
+    displayOutput(contacts);
 }
